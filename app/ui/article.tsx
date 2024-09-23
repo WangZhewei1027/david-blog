c479fda8b206e1358d0d39ce12eed418c1d92251
王哲玮 <m13636472690@163.com>
2024-09-23 01:16:48 -0400

    Update article.tsx

--- app/ui/article.tsx
+++ app/ui/article.tsx
@@ -9,11 +9,9 @@ export default function Article({ title, date, caption, link }: ArticleProps) {
   return (
     <div className="hover:bg-gray-100 py-6 px-2 border-b transition-all sm:p-8">
       <a href={link} className="no-underline">
-        <h2 className="text-base font-bold mb-1 sm:text-xl">
-          <li>{title}</li>
-        </h2>
+        <h2 className="text-base font-bold mb-1 sm:text-xl">{title}</h2>
         <p className="text-sm text-gray-500 m-0 sm:text-base">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{date} &nbsp;&nbsp;{caption}
+          {date} &nbsp;&nbsp;{caption}
         </p>
       </a>
     </div>
