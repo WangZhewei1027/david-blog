8e7a965ed1617f47b5f791f09497f205297415bd
王哲玮 <m13636472690@163.com>
2024-09-22 20:59:51 -0400

    Update container.tsx

--- app/ui/container.tsx
+++ app/ui/container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-3xl mx-auto px-4 min-h-screen justify-center pt-8 sm:px-6 sm:pt-10 lg:px-8">
+    <div className="max-w-3xl mx-auto px-4 py-4 min-h-screen justify-center pt-8 sm:px-6 sm:pt-10 lg:px-8">
       {children}
     </div>
   );
