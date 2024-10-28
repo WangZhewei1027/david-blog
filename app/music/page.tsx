85a1b47650492268fffb6526a2682f8931a7ad68
王哲玮 <m13636472690@163.com>
2024-10-28 17:22:24 -0400

    Update page.tsx

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,5 +1,11 @@
 import Container from "../ui/container";
 
+const albums = [
+  "7lxl8IT9CF5fh7pTN1CHYD",
+  "68hTTrAwPFA0dWrxkAUMVl",
+  "4AwRYl2kRyCYaAkxHAklZ8",
+];
+
 export default function Page() {
   return (
     <>
@@ -7,16 +13,22 @@ export default function Page() {
         <h1 className="text-4xl font-bold">Music Page</h1>
         <p className="mt-4 text-lg">Welcome to the music page!</p>
         <h2 className="text-3xl font-bold">Recent Listening</h2>
-        <iframe
-          style={{ borderRadius: "12px" }}
-          src="https://open.spotify.com/embed/album/7lxl8IT9CF5fh7pTN1CHYD?utm_source=generator"
-          width="100%"
-          height="152"
-          frameBorder="0"
-          allowFullScreen
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        ></iframe>
+        <div className="space-y-2">
+          {albums.map((album) => (
+            <>
+              <iframe
+                style={{ borderRadius: "12px" }}
+                src={`https://open.spotify.com/embed/album/${album}?utm_source=generator`}
+                width="100%"
+                height="152"
+                frameBorder="0"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              ></iframe>
+            </>
+          ))}
+        </div>
       </Container>
     </>
   );
