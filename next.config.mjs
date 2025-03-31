6feb4a7464b47a24d40532c387788d3285c16590
王哲玮 <m13636472690@163.com>
2025-03-30 21:55:05 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 import createMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import { visit } from "unist-util-visit";
+import pkg from "next/dist/compiled/webpack/webpack-lib.js"; // 默认导入
+const { IgnorePlugin } = pkg;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
+  // Configure pageExtensions to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+  // 其它配置项...
   webpack: (config, { isServer }) => {
     if (isServer) {
       // 忽略 public/posts 目录下的图片和视频文件
       config.plugins.push(
-        new webpack.IgnorePlugin({
-          // 匹配文件扩展名：图片和视频
+        new IgnorePlugin({
           resourceRegExp: /\.(png|jpe?g|gif|svg|mp4|webm)$/,
-          // 只对 public/posts 目录下的文件生效
           contextRegExp: /public\/posts/,
         })
       );
